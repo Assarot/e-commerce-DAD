531e8dbbd1fcfa8b3c2c77eb1f0c8c09df8c138c
Jherson <jhersonccanto@upeu.edu.pe~>
2025-04-21 11:34:25 -0500

    Limpieza para recrear microservicio en rama propia

--- services/ms-category-service/src/main/java/pe/edu/upeu/ms_category_service/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.ms_category_service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upeu.ms_category_service.domain.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-}
